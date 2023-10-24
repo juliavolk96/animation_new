a225321ac127be450bc5963362669afdc9868b12
Julia <juliavolk96@gmail.com>
2023-10-24 19:41:42 +0400

    fix erorrs

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 // TODO: write code here
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   const button = document.querySelector('.collapsible-button');
   const container = document.querySelector('.collapsible-container');
 
   let isCollapsed = true;
 
-  button.addEventListener('click', function () {
+  button.addEventListener('click', () => {
     if (isCollapsed) {
-      container.style.maxHeight = container.scrollHeight + 'px';
+      container.style.maxHeight = `${container.scrollHeight}px`;
     } else {
       container.style.maxHeight = '0';
     }
